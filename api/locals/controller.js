757195210dc0c55e691ba3290cf5dd53ce0b161d
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-19 11:12:44 +0000

    ok

--- api/locals/controller.js
+++ api/locals/controller.js
@@ -3,6 +3,8 @@
 //Mostrar los locales solicitales 
 //Actualizar las características de los locales
 //Eliminar locales? Depende
+
+//PRIMERA PRUEBA: usando por defecto datos de locales. 
 const locals = [{ name: 'Local Alfonso', id: 1, location: "C/ Teresa de los campos" }, { name: 'Local Gigants', id: 2, location: "C/ Pedro de Alonso" }];
 
 function getLocals(){
