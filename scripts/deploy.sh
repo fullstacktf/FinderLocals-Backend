f8e074c6c924cc1262cbc6f76e51efd880511ab7
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-21 11:38:55 +0000

    nueva prueba 7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo "Deploying on digitalocean"  
 
-COMMAND="cd ${REMOTE_PATH} && git branch && git status && git checkout digitalocean && git pull"
+COMMAND="cd ${REMOTE_PATH} && git branch && git status && git checkout digitalocean && git pull --force"
 
 ssh -o StrictHostKeyChecking=no -i sshTravis -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
