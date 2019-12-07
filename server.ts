ba0fef9c063c881250de715033be78fe387f7ea9
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-12-07 18:45:45 +0000

    increasing numbers of gets

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ const corsOptions = {
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20,
+  max: 50,
   message: "Too many requests,  please try again after 15 min"
 });
 
